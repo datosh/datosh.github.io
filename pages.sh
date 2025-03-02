4cb14d3ac05a22adce9dde7e45c0d6e4cd4d7411
Fabian Kammel <fabian@kammel.dev>
2025-03-02 15:03:17 +0100

    use correct base URL for prod
    Signed-off-by: Fabian Kammel <fabian@kammel.dev>

--- pages.sh
+++ pages.sh
@@ -3,7 +3,7 @@
 ./fetch_goinvaders.sh
 
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
-  hugo --baseURL $CF_PAGES_URL
+  hugo --baseURL blog.kammel.dev
 else
   hugo --baseURL $CF_PAGES_URL --buildDrafts --buildFuture
 fi
