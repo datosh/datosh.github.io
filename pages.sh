279371db7f9cd31a0826f60ee9f60035994dd176
Fabian Kammel <fabian@kammel.dev>
2025-03-02 15:06:07 +0100

    fix: prod base url
    Signed-off-by: Fabian Kammel <fabian@kammel.dev>

--- pages.sh
+++ pages.sh
@@ -3,7 +3,7 @@
 ./fetch_goinvaders.sh
 
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
-  hugo --baseURL blog.kammel.dev
+  hugo --baseURL "https://blog.kammel.dev/"
 else
   hugo --baseURL $CF_PAGES_URL --buildDrafts --buildFuture
 fi
